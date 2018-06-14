6673b0570bd3183c2ca47938e144d156b3be29ab
Marcos de Matos <marck010@hotmail.com>
2018-06-14 03:56:29 -0300

    Alterado saldo diario

--- Service/docker-entrypoint.sh
+++ Service/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 export SERVER_DB=ds257470.mlab.com:57470/snack-machine
 export USER_DB=user_snack_machine
 export PASS_DB=g6WzsDeDsb2trs56*
-export DEFAULT_BALANCE=10
+export DEFAULT_BALANCE=5.50
 
 npm install
 bin/www 
